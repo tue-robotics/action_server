42a3fe4248d58e27b937a7a48e157c17702fa433
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2015-11-18 13:16:05 +0100

    enhance(speech_client): Updated speech client for Klokhuis demo

--- clients/speech_client.py
+++ clients/speech_client.py
@@ -5,6 +5,8 @@ from robot_skills.ears import Ears
 from robot_skills.speech import Speech
 from robot_skills.head import Head
 
+import robot_skills.util.msg_constructors as msgs
+
 import random
 import time
 
@@ -35,31 +37,47 @@ rospy.wait_for_service('/%s/action_server/add_action'%robot_name)
 
 e = robot.ears
 s = robot.speech
-robot.head.look_at_standing_person()
 
 drinks = ['coke','fanta']
 places = ['table','cabinet']
 
 def ask():
-    s.speak("Say %s and I will listen to you!"%robot_name)
-    spec = robot_name
-    r = e.recognize(spec, {}, time_out=rospy.Duration(60))
+    s.speak("Hello! What can I do for you?")
 
-    if r:
-        s.speak("What's up")
-        spec = "Bring me a <drink> from the <place>"
+    while not rospy.is_shutdown():
+        spec = "Bring me the <drink> from the <place>"
         choices = {"drink" : drinks, "place" : places}
         r = e.recognize(spec, choices)
-        if r:
-            s.speak("You said: %s"%r.result)
-            try:
-                add_action = rospy.ServiceProxy('/%s/action_server/add_action'%robot_name, AddAction)
-                add_action(action=r.choices["drink"], parameters="{entity:%s}"%r.choices['place'])
-                sys.exit()
-            except rospy.ServiceException, error:
-                print "Service call failed: %s"%error
-        else:
+        if not r:
             s.speak("I didn't quite get that")
+            return
+
+        s.speak("Do you want me to bring you the %s from the %s?" % (r.choices["drink"], r.choices["place"]))
+        r = e.recognize("<yesno>", {"yesno": ["yes", "no"]})
+        if not r:
+            s.speak("I'll take that as a yes")
+            break
+        elif r.choices["yesno"] == "no":
+            s.speak("I'm sorry I didn't understand! Can you please tell me again?")
+        else:
+            s.speak("Alright!")
+            break
+
+    # Cancel the head goal
+    robot.head.cancel_goal()
+
+    try:
+        add_action = rospy.ServiceProxy('/%s/action_server/add_action'%robot_name, AddAction)
+        #add_action(action=r.choices["drink"], parameters="{entity:%s}"%r.choices['place'])
+
+        params = {"entity": {}, "from": {"id": "dinnertable"}, "from_room" : "living_room",
+                  "to": {"id": "operator"}, "to_radius": 1.5}
+
+        import json
+        add_action(action="bring", parameters=json.dumps(params))
+        sys.exit()
+    except rospy.ServiceException, error:
+        print "Service call failed: %s"%error
 
 while not rospy.is_shutdown():
     ask()
