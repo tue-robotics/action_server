8839b0228d712a0a5e0911265d099651fd378a57
Loy van Beek <loy.vanbeek@gmail.com>
2017-01-14 17:31:33 +0100

    Use Entity.distance_to_2d to sort by

--- src/action_server/actions/util/entities_from_description.py
+++ src/action_server/actions/util/entities_from_description.py
@@ -40,9 +40,6 @@ def entities_from_description(entity_descr, robot):
     robot_pos = robot.base.get_location().pose.position
 
     # Sort entities by distance
-    entities = sorted(entities,
-                      key=lambda entity: length_sq(
-                          robot_pos.x - entity.pose.position.x,
-                          robot_pos.y - entity.pose.position.y))
+    entities = sorted(entities, key=lambda entity: entity.distance_to_2d(robot_pos))
 
     return (entities, "")
