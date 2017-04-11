8899b42c052d3f3f611048d411ea9799e1b65453
Rokus Ottervanger <rokusottervanger@gmail.com>
2017-04-11 19:50:24 +0200

    fix(reset_wm): typo in configure method

--- src/action_server/actions/reset_wm.py
+++ src/action_server/actions/reset_wm.py
@@ -2,9 +2,11 @@ from action import Action
 
 
 class ResetWM(Action):
+    def __init__(self):
+        Action.__init__(self)
 
-    def _configure_(self, robot, config):
-        self._robot = robot # TODO: this should also check if the given robot is capable of this action.
+    def _configure(self, robot, config):
+        self._robot = robot  # TODO: this should also check if the given robot is capable of this action.
 
         self._config_result.succeeded = True
         return
