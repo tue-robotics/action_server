8263b4894f2265b3cfc73c559ff3220aa0810890
Rokus Ottervanger <rokusottervanger@gmail.com>
2017-05-03 15:50:54 +0200

    feat(actions): add answer_question action

---
+++ src/action_server/actions/answer_question.py
@@ -0,0 +1,76 @@
+from action import Action
+
+import rospy
+from robocup_knowledge import load_knowledge
+
+class AnswerQuestion(Action):
+    def __init__(self):
+        Action.__init__(self)
+
+    def _configure(self, robot, config):
+        if not hasattr(robot, "speech"):
+            rospy.logerr("Robot {} does not have attribute 'speech'".format(robot.robot_name))
+            self._config_result.missing_skill = "speech"
+            return
+
+        if not hasattr(robot, "ears"):
+            rospy.logerr("Robot {} does not have attribute 'ears'".format(robot.robot_name))
+            self._config_result.missing_skill = "ears"
+            return
+
+        self._robot = robot
+
+        self._speech_data = load_knowledge('challenge_speech_recognition')
+        if not self._speech_data:
+            rospy.logerr("Failed to load speech data for 'AnswerQuestion' action")
+            return
+
+        self._config_result.succeeded = True
+
+    def _start(self):
+        self._robot.head.look_at_standing_person()
+        self._robot.head.wait_for_motion_done()
+
+        self._robot.speech.speak("What is your question?")
+
+        res = self._robot.ears.recognize(spec=self._speech_data.spec,
+                                         choices=self._speech_data.choices,
+                                         time_out=rospy.Duration(15))
+
+        if not res:
+            self._robot.speech.speak("My ears are not working properly, sorry!")
+            self._execute_result.message = "was unable to hear anything when listening for a challenge"
+            return
+
+        if "question" in res.choices:
+            rospy.loginfo("Question was: '%s'?" % res.result)
+            self._robot.speech.speak("The answer is %s" % self._speech_data.choice_answer_mapping[res.choices['question']])
+            self._execute_result.message = "answered the question {}".format(res.choices['question'])
+            self._execute_result.succeeded = True
+        else:
+            self._robot.speech.speak("Sorry, I did not understand your question.")
+            self._execute_result.message = "did not understand the question"
+
+    def _cancel(self):
+        pass
+
+if __name__ == "__main__":
+    rospy.init_node('answer_question_test')
+
+    import sys
+    robot_name = sys.argv[1]
+    if robot_name == 'amigo':
+        from robot_skills.amigo import Amigo as Robot
+    elif robot_name == 'sergio':
+        from robot_skills.sergio import Sergio as Robot
+    else:
+        from robot_skills.mockbot import Mockbot as Robot
+
+    robot = Robot()
+
+    action = AnswerQuestion()
+
+    config = {'action': 'answer_question'}
+
+    action.configure(robot, config)
+    action.start()
