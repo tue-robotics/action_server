8801d42d4151bccf75af5b5cc8250c1fecc9293d
Rokus Ottervanger <rokusottervanger@gmail.com>
2017-05-03 16:46:02 +0200

    fix(actions): only put working actions in __init__.py

--- src/action_server/actions/__init__.py
+++ src/action_server/actions/__init__.py
@@ -1,11 +1,13 @@
 # List of all skills/actions that are going to be registered
 
+from answer_question import AnswerQuestion
 from arm_goal import ArmGoal
-from bring import Bring
+# from bring import Bring
 from gripper_goal import GripperGoal
 from inspect import Inspect
 from look_at import LookAt
 from navigate_to import NavigateTo
-from pick_up import PickUp
-from place import Place
+# from pick_up import PickUp
+# from place import Place
 from reset_wm import ResetWM
+from find import Find
