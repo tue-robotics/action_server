4de8b01f3cdf990e3650c41a203eb9901517c272
Rokus Ottervanger <rokusottervanger@gmail.com>
2017-07-27 13:34:59 +0900

    feat(answer-question): first find a person if given in task

--- src/action_server/actions/answer_question.py
+++ src/action_server/actions/answer_question.py
@@ -1,4 +1,5 @@
-from action import Action
+from action import Action, ConfigurationData
+from find import Find
 
 import rospy
 from robocup_knowledge import load_knowledge
@@ -25,34 +26,67 @@ class AnswerQuestion(Action):
             rospy.logerr("Failed to load speech data for 'AnswerQuestion' action")
             return
 
+        # TODO: If no target-person is given in semantics, check if there is someone else to answer the question to
+        # for example using a person from config.knowledge
+
+        # If a person is specified in the task description, we need to go and find that person first
+        if 'target-person' in config.semantics:
+            self._find_action = Find()
+            location_id = config.semantics['target-person']['loc']
+            find_semantics = {'object' : config.semantics['target-person'],
+                              'location' : {'id': location_id,
+                                            'type': "furniture" if self._knowledge.is_location(location_id)
+                                            else "room"}}
+            find_config = ConfigurationData(find_semantics, config.knowledge)
+            find_config_result = self._find_action.configure(robot, find_config)
+            if not find_config_result.succeeded:
+                self._config_result = find_config_result
+                return
+        else:
+            self._find_action = None
+
         self._config_result.succeeded = True
 
     def _start(self):
+        if self._find_action:
+            find_exec_res = self._find_action.start()
+            if not find_exec_res.succeeded:
+                self._execute_result = find_exec_res
+                return
+
         self._robot.head.look_at_standing_person()
         self._robot.head.wait_for_motion_done()
 
         self._robot.speech.speak("What is your question?")
 
-        try:
-            res = self._robot.hmi.query(description="",
-                                        grammar=self._speech_data.grammar,
-                                        target=self._speech_data.grammar_target)
-        except hmi.TimeoutException:
-            self._robot.speech.speak("My ears are not working properly, sorry!")
-            self._execute_result.message = " I was unable to hear anything when listening for a question, so I could " \
-                                           "not answer it. "
-            return
+        tries = 0
+        while tries < 3:
+            try:
+                res = self._robot.hmi.query(description="",
+                                            grammar=self._speech_data.grammar,
+                                            target=self._speech_data.grammar_target)
+            except hmi.TimeoutException:
+                self._robot.speech.speak("My ears are not working properly, sorry!")
+                self._execute_result.message = " I was unable to hear anything when listening for a question, so I could " \
+                                               "not answer it. "
+                return
 
-        print res.semantics
+            print res.semantics
 
-        if res.semantics and "actions" in res.semantics:
-            rospy.loginfo("Question was: '%s'?" % res.sentence)
-            self._robot.speech.speak("The answer is %s" % res.semantics['actions'][0]['solution'])
-            self._execute_result.message = " I answered the question {}".format(res.sentence)
-            self._execute_result.succeeded = True
-        else:
-            self._robot.speech.speak("Sorry, I did not understand your question.")
-            self._execute_result.message = " I did not understand the question"
+            if res.semantics and "actions" in res.semantics:
+                rospy.loginfo("Question was: '%s'?" % res.sentence)
+                self._robot.speech.speak("The answer is %s" % res.semantics['actions'][0]['solution'])
+                self._execute_result.message = " I answered the question {}".format(res.sentence)
+                self._execute_result.succeeded = True
+                return
+            else:
+                if tries < 2:
+                    self._robot.speech.speak("Sorry, I did not understand your question, try another one.")
+                else:
+                    self._robot.speech.speak("Sorry, I was unable to understand any of your questions. I'll leave you puzzled by them. ")
+                    self._execute_result.message = " I did not understand the question. "
+                tries += 1
+
 
     def _cancel(self):
         pass
