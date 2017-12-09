5a94972052a278e027b9e2442ed8d461fad90d0d
Rokus Ottervanger <rokusottervanger@gmail.com>
2017-12-09 14:40:48 +0100

    log(server): add task recipe to stack trace

--- action_server/src/action_server/server.py
+++ action_server/src/action_server/server.py
@@ -36,47 +36,51 @@ class Server(object):
     def _add_action_cb(self, goal):
         recipe = yaml.load(goal.recipe)
         rospy.logdebug("Received action recipe: {}".format(goal.recipe))
-        configuration_result = self._task_manager.set_up_state_machine(recipe['actions'])
-        rospy.logdebug("Result of state machine setup: {}".format(configuration_result))
 
-        self._result.log_messages = []
+        try:
+            configuration_result = self._task_manager.set_up_state_machine(recipe['actions'])
+            rospy.logdebug("Result of state machine setup: {}".format(configuration_result))
 
-        if configuration_result.succeeded:
-            rospy.logdebug("Setting up state machine succeeded")
-        else:
-            if configuration_result.missing_field:
-                self._result.result = action_server_msgs.msg.TaskResult.RESULT_MISSING_INFORMATION
-                self._result.missing_field = "actions" + configuration_result.missing_field
-                self._result.log_messages.append(" I don't have enough information to perform that task.")
-                self._result.log_messages.append(configuration_result.message)
-            elif configuration_result.message:
-                # TODO: this task result should not be RESULT_UNKNOWN
-                self._result.result = action_server_msgs.msg.TaskResult.RESULT_UNKNOWN
-                self._result.log_messages.append(configuration_result.message)
+            self._result.log_messages = []
+
+            if configuration_result.succeeded:
+                rospy.logdebug("Setting up state machine succeeded")
             else:
-                self._result.result = action_server_msgs.msg.TaskResult.RESULT_UNKNOWN
-                self._result.log_messages.append(" It seems that I am unable to perform that task. "
-                                                   "Not sure why though.")
-            self._action_server.set_aborted(self._result)
-            self._task_manager.clear()
-            rospy.logerr("Setting up state machine failed")
-            return
-
-        while not self._task_manager.done:
-            # Pass feedback to client about what type of action is running
-            feedback = action_server_msgs.msg.TaskFeedback()
-            feedback.current_subtask = self._task_manager.get_next_action_name()
-            self._action_server.publish_feedback(feedback)
-
-            action_result = self._task_manager.execute_next_action()
-            rospy.logdebug("Result of action execution: {}".format(action_result))
-            self._result.log_messages.append(action_result.message)
-            if not action_result.succeeded:
-                self._result.result = action_server_msgs.msg.TaskResult.RESULT_TASK_EXECUTION_FAILED
+                if configuration_result.missing_field:
+                    self._result.result = action_server_msgs.msg.TaskResult.RESULT_MISSING_INFORMATION
+                    self._result.missing_field = "actions" + configuration_result.missing_field
+                    self._result.log_messages.append(" I don't have enough information to perform that task.")
+                    self._result.log_messages.append(configuration_result.message)
+                elif configuration_result.message:
+                    # TODO: this task result should not be RESULT_UNKNOWN
+                    self._result.result = action_server_msgs.msg.TaskResult.RESULT_UNKNOWN
+                    self._result.log_messages.append(configuration_result.message)
+                else:
+                    self._result.result = action_server_msgs.msg.TaskResult.RESULT_UNKNOWN
+                    self._result.log_messages.append(" It seems that I am unable to perform that task. "
+                                                       "Not sure why though.")
                 self._action_server.set_aborted(self._result)
                 self._task_manager.clear()
-                rospy.logdebug("Execution of state machine aborted because action failed.")
+                rospy.logerr("Setting up state machine failed")
                 return
+
+            while not self._task_manager.done:
+                # Pass feedback to client about what type of action is running
+                feedback = action_server_msgs.msg.TaskFeedback()
+                feedback.current_subtask = self._task_manager.get_next_action_name()
+                self._action_server.publish_feedback(feedback)
+
+                action_result = self._task_manager.execute_next_action()
+                rospy.logdebug("Result of action execution: {}".format(action_result))
+                self._result.log_messages.append(action_result.message)
+                if not action_result.succeeded:
+                    self._result.result = action_server_msgs.msg.TaskResult.RESULT_TASK_EXECUTION_FAILED
+                    self._action_server.set_aborted(self._result)
+                    self._task_manager.clear()
+                    rospy.logdebug("Execution of state machine aborted because action failed.")
+                    return
+        except Exception as e:
+            raise type(e), type(e)("An error occurred using task recipe: %s\n" % goal.recipe + e.message)
 
         rospy.logdebug("Execution of state machine succeeded.")
         self._result.result = action_server_msgs.msg.TaskResult.RESULT_SUCCEEDED
