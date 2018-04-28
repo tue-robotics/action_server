db109f6c9981cad04ec5bbffae6034107e58861f
Rokus Ottervanger <rokusottervanger@gmail.com>
2018-04-28 21:56:10 +0200

    fix(guide): guide someone to the bar

--- action_server/src/action_server/actions/guide.py
+++ action_server/src/action_server/actions/guide.py
@@ -1,38 +1,47 @@
-from action import ConfigurationData
-from navigate_to import NavigateTo
+from action import Action, ConfigurationData
+import robot_smach_states as states
+import robot_smach_states.util.designators as ds
 
 import rospy
 
 
-class Guide(NavigateTo):
+class Guide(Action):
     ''' The Guide class navigates to a target, telling someone to follow the robot and about arriving at the target.
 
     Parameters to pass to the configure() method are:
      - `object` (required): the id of the entity to navigate to
     '''
     def __init__(self):
-        NavigateTo.__init__(self)
+        Action.__init__(self)
+        self._required_field_prompts = {'object': " What exactly would you like me to find? "}
+        self._required_skills = ['head', 'base', 'rightArm', 'speech']
 
     def _configure(self, robot, config):
         if 'object' in config.semantics and 'id' in config.semantics['object']:
-            self._id = config.semantics['object']['id']
+            self.follower_id = config.semantics['object']['id']
         else:
-            self._id = None
+            # TODO: HUUUUGE Robocup hack!
+            self.follower_id = "lars"
 
-        return NavigateTo._configure(self, robot, config)
+        if 'target-location' in config.semantics and 'id' in config.semantics['target-location']:
+            self.target_location_id = config.semantics['target-location']['id']
+        else:
+            # TODO: HUUUUGE Robocup hack!
+            self.target_location_id = "bar"
+
+        target_location_designator = ds.EntityByIdDesignator(robot, id=self.target_location_id)
+        follower_designator = ds.EntityByIdDesignator(robot, id=self.follower_id)
+
+        self._guide_state_machine = states.Guide(robot=robot, target_location=target_location_designator, follower=follower_designator)
+        self._config_result.succeeded = True
 
     def _start(self):
-        self._robot.speech.speak("Follow me if you want to live", block=True)
-
-        res = NavigateTo._start(self)
-
-        if self._id:
-            self._robot.speech.speak("Here is the {}".format(self._id))
-
-        return res
+        self._guide_state_machine.execute()
+        self._execute_result.succeeded = True
+        self._execute_result.message = "I guided "
 
     def _cancel(self):
-        return NavigateTo._cancel(self)
+        self._guide_state_machine.request_preempt()
 
 
 if __name__ == "__main__":
@@ -49,7 +58,7 @@ if __name__ == "__main__":
 
     robot = Robot()
 
-    action = NavigateTo()
+    action = Guide()
 
     config = ConfigurationData({'action': 'guide',
               'object': {'id': 'cabinet'}})
