61ea670b73f93678103dda8bd19c627d1a4994b7
Rokus Ottervanger <rokusottervanger@gmail.com>
2018-06-20 11:52:32 -0400

    fix(guide): target is required

--- action_server/src/action_server/actions/guide.py
+++ action_server/src/action_server/actions/guide.py
@@ -14,7 +14,8 @@ class Guide(Action):
     '''
     def __init__(self):
         Action.__init__(self)
-        self._required_field_prompts = {'object': " Who would you like me to guide? "}
+        self._required_field_prompts = {'object': " Who would you like me to guide? ",
+                                        'target-location': " Where would you like me to guide them? "}
         self._required_skills = ['head', 'base', 'rightArm', 'speech']
         self._follower_id = None
         self._target_id = None
