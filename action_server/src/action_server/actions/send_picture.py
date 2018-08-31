021906da7dfae1ef10a1605f7ff078677c440a82
josja_geijsberts <j.geijsberts@student.tue.nl>
2018-08-31 16:43:53 +0200

    changed send_picture action

--- action_server/src/action_server/actions/send_picture.py
+++ action_server/src/action_server/actions/send_picture.py
@@ -4,7 +4,7 @@ import robot_smach_states as states
 from sensor_msgs.msg import Image
 
 from entity_description import resolve_entity_description
-
+from robot_smach_states.util.designators import EdEntityDesignator
 
 class SendPicture(Action):
     """ The Inspect class implements the action to send a picture over a whatsapp client.
@@ -56,8 +56,9 @@ class SendPicture(Action):
         # self.detect_face_state_machine = states.DetectFace(self._robot)
 
         entity_to_inspect = resolve_entity_description(config.semantics['target-location'])
-
-        self.look_at_sm = states.LookOnTopOfEntity(self._robot, entity_to_inspect)
+	entity_designator = EdEntityDesignator(self._robot, id=entity_to_inspect.id)
+	entity = entity_designator.resolve()
+        self.look_at_sm = states.LookOnTopOfEntity(self._robot, entity)
 
         self._config_result.succeeded = True
         return
@@ -65,11 +66,11 @@ class SendPicture(Action):
     def _start(self):
         self._robot.speech.speak("I will take a picture and send it to my operator now. ")
 
-        image_pub = rospy.Publisher("/hero/photo_to_telegram", Image, queue_size=1)
-        self.look_at_sm.run(self._robot)
+        image_pub = rospy.Publisher("/hero/image_from_ros", Image, queue_size=1)
+        self.look_at_sm.run()
 
-        image_pub.publish(robot.perception.get_image())
-        robot.head.close()
+        image_pub.publish(self._robot.perception.get_image())
+        self._robot.head.close()
 
         # if result == 'failed':
         #     self._execute_result.message = " I didn't see anyone. "
@@ -79,9 +80,7 @@ class SendPicture(Action):
         self._execute_result.succeeded = True
 
     def _cancel(self):
-        if self.detect_face_state_machine.is_running:
-            self.detect_face_state_machine.request_preempt()
-
+	pass
 
 if __name__ == "__main__":
     rospy.init_node('send_picture_test')
