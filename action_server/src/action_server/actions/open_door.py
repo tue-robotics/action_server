c6159a9a32344d430cf1cbb498f7c0434a144ee8
Josja Geijsberts <j.geijsberts@student.tue.nl>
2019-04-06 14:28:06 +0200

    general setup

--- action_server/src/action_server/actions/open_door.py
+++ action_server/src/action_server/actions/open_door.py
@@ -1,6 +1,6 @@
 from action import Action, ConfigurationData
-from challenge_storing_groceries.open_door import OpenDoorMachine
-
+from robot_smach_states.manipulation import OpenDoor as OpenDoorMachine
+from entity_description import resolve_entity_description
 
 class OpenDoor(Action):
     """
@@ -10,19 +10,76 @@ class OpenDoor(Action):
     def __init__(self):
         # Call the base class constructor
         Action.__init__(self)
-        # self._required_field_prompts = {'object': " What object would you like me to work with? "}
+        self._required_field_prompts = {'furniture_id':
+                                            " From what piece of furniture would you like me to open the door? "}
+        self._required_field_prompts = {'furniture_navigate_area':
+                                            " Where near that piece of furniture should I navigate to? "}
+        self.furniture_inspect_area = {'furniture_inspect_area':
+                                           " What near that piece of furniture can be used to inspect? "}
+
+    class Semantics:
+        def __init__(self):
+            self.furniture_id = None
+            self.furniture_navigate_area = None
+            self.furniture_inspect_area = None
+
+    @staticmethod
+    def _parse_semantics(semantics_dict):
+        semantics = OpenDoor.Semantics()
+
+        if 'furniture_id' in semantics_dict:
+            semantics.furniture_id = resolve_entity_description(semantics_dict['furniture_id'])
+        if 'furniture_navigate_area' in semantics_dict:
+            semantics.furniture_navigate_area = resolve_entity_description(semantics_dict['furniture_navigate_area'])
+        if 'furniture_inspect_area' in semantics_dict:
+            semantics.furniture_inspect_area = resolve_entity_description(semantics_dict['furniture_inspect_area'])
+
+        return semantics
 
     def _configure(self, robot, config):
-        self._state_machine = OpenDoorMachine(robot, "cupboard", "in_front_of", "shelf6")
+        self._robot = robot
+
+        semantics = OpenDoor._parse_semantics(config.semantics)
+
+        self._state_machine = OpenDoorMachine(self._robot, semantics.furniture_id,
+                                              semantics.furniture_navigate_area, semantics.furniture_inspect_area)
         self._config_result.succeeded = True
-        return
 
     def _start(self):
-        self._state_machine.execute()
-
-        self._execute_result.message = " I opened the cupboard door. "
-        self._execute_result.succeeded = True
+        self._result = self._state_machine.execute()
+        if self._result == "succeeded":
+            self._execute_result.message = " I opened a door. "
+            self._execute_result.succeeded = True
+        else:
+            self._execute_result.message = " I could not open a door. "
+            self._execute_result.succeeded = False
         return
 
     def _cancel(self):
         pass
+
+if __name__ == "__main__":
+    rospy.init_node('open_door_test')
+
+    import sys
+    robot_name = sys.argv[1]
+    if robot_name == 'amigo':
+        from robot_skills.amigo import Amigo as Robot
+    elif robot_name == 'sergio':
+        from robot_skills.sergio import Sergio as Robot
+    elif robot_name == 'hero':
+        from robot_skills.sergio import Hero as Robot
+    else:
+        from robot_skills.mockbot import Mockbot as Robot
+
+    robot = Robot()
+
+    action = OpenDoor()
+
+    config = ConfigurationData({'action': 'open-door',
+                                'furniture_id': 'cabinet',
+                                'furniture_navigate_area': 'in_front_of',
+                                'furniture_inspect_area': 'on_top_of'})
+
+    action.configure(robot, config)
+    action.start()
