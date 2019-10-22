8908357655d6b325c6cf7bef41fdae1f95f2ad4b
Janno Lunenburg <jannolunenburg@gmail.com>
2019-10-22 19:53:27 +0200

    cleanup(action)Server pep8ified some stuff, removed deprecated code

--- action_server/src/action_server/actions/util/entities_from_description.py
+++ action_server/src/action_server/actions/util/entities_from_description.py
@@ -1,33 +1,35 @@
-import rospy
 from robot_skills.robot import Robot
 
-def length_sq(x, y):
-    return x * x + y * y
-
-# ----------------------------------------------------------------------------------------------------
 
 def entities_from_description(entity_descr, robot):
-    '''
+    """
     Query entities with various methods
 
-    @param entity_descr: A dict that contains an 'id' or 'type' field
+    @param entity_descr: A dict that contains an "id" or "type" field
     @param robot: The robot object
 
     @return: (entities, error_msg)
         entities  - list of entities that fulfill the description
                     (each element has type EntityInfo)
         error_msg - If something goes wrong, this contains the message
-    '''
+    """
     if not isinstance(entity_descr, dict):
-        return ([], "entities_from_description: the specified entity_descr should be a dictionary! I received: %s" % str(entity_descr))
+        return (
+            [],
+            "entities_from_description: the specified entity_descr should be a dictionary!"
+            "I received: %s" % str(entity_descr)
+        )
 
     if not isinstance(robot, Robot):
-        return ([], "entities_from_description: the specified robot should be a Robot! I received: %s" % str(robot))
+        return (
+            [],
+            "entities_from_description: the specified robot should be a Robot! I received: %s" % str(robot)
+        )
 
     if "id" in entity_descr:
         e = robot.ed.get_entity(id=entity_descr["id"], parse=False)
         if not e:
-            return ([], "No entity with id '%s'" % entity_descr["id"])
+            return [], "No entity with id '%s'" % entity_descr["id"]
         entities = [e]
     elif "type" in entity_descr:
         entities = robot.ed.get_entities(type=entity_descr["type"], parse=False)
@@ -42,10 +44,11 @@ def entities_from_description(entity_descr, robot):
                 area = entity_descr["area"]
             else:
                 area = "on_top_of"
-            entities = [e for e in entities if location_entity.in_volume(volume_id=area, point=e.pose.extractVectorStamped())]
+            entities = [e for e in entities if location_entity.in_volume(
+                volume_id=area, point=e.pose.extractVectorStamped())]
 
     if not entities:
-        return ([], "No such entity")
+        return [], "No such entity"
 
     robot_location = robot.base.get_location()
     robot_pos = robot_location.frame.p
@@ -53,4 +56,4 @@ def entities_from_description(entity_descr, robot):
     # Sort entities by distance
     entities = sorted(entities, key=lambda entity: entity.distance_to_2d(robot_pos))
 
-    return (entities, "")
+    return entities, ""
