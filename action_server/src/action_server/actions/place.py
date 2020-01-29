1ce1fb6ef3c218ac4c4c2be6692427f90b0444ea
Peter van Dooren <pepevandooren@hotmail.com>
2020-01-29 16:43:58 +0100

    changed input to the place smach state to not use an emptyspotdesignator

--- action_server/src/action_server/actions/place.py
+++ action_server/src/action_server/actions/place.py
@@ -141,14 +141,11 @@ class Place(Action):
         item_to_place = robot_smach_states.util.designators.Designator(arm_designator.resolve().occupied_by,
                                                                        resolve_type=Entity)
 
-        place_position = robot_smach_states.util.designators.EmptySpotDesignator(
-            self._robot,
-            robot_smach_states.util.designators.EdEntityDesignator(self._robot, id=self.context.location.id),
-            area="on_top_of"
-        )
-
-        self._place = PlaceSmachState(self._robot, item_to_place, place_position,
-                                      arm_designator)
+        entity_to_place_on = robot_smach_states.util.designators.EdEntityDesignator(self._robot, id=self.context.location.id)
+        self._place = PlaceSmachState(self._robot, item_to_place,
+                                      entity_to_place_on,
+                                      arm_designator,
+                                      place_volume="on_top_of")
 
         state_machine_result = self._place.execute()
         if state_machine_result == 'done':
